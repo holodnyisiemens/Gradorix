92b03314e562c1fad1f396ae020b5f5baaf4eb15
holodnyisiemens <idolexey@yandex.ru>
2026-02-25 18:54:22 +0500

    Add schema for notification

---
+++ app/schemas/notification.py
@@ -0,0 +1,24 @@
+from annotated_types import MaxLen
+from typing import Annotated, Optional
+
+from app.schemas.base import BaseDTO
+
+
+class NotificationCreateDTO(BaseDTO):
+    user_id: int
+    message: Annotated[str, MaxLen(1000)]
+
+    is_read: Optional[bool]
+
+
+class NotificationReadDTO(BaseDTO):
+    id: int
+    user_id: int
+    message: str
+    is_read: bool
+
+
+class NotificationUpdateDTO(BaseDTO):
+    user_id: Optional[int] = None
+    message: Optional[Annotated[str, MaxLen(1000)]] = None
+    is_read: Optional[bool] = None
