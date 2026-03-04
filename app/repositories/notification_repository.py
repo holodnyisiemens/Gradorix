b1f0d60ab6389df5a46af9fd43da73c69f13bd13
Alexey <idolexey@yandex.ru>
2026-03-05 01:11:22 +0500

    Add notification repo

---
+++ app/repositories/notification_repository.py
@@ -0,0 +1,43 @@
+from typing import Optional
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models.notification import Notification
+from app.schemas.notification import NotificationCreateDTO, NotificationUpdateDTO
+
+
+class NotificationRepository:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def get_by_id(self, notification_id: int) -> Optional[Notification]:
+        return await self.session.get(Notification, notification_id)
+
+    async def create(self, notification_data: NotificationCreateDTO) -> Notification:
+        notification = Notification(**notification_data.model_dump())
+        self.session.add(notification)
+
+        await self.session.flush()
+        await self.session.refresh(notification)
+
+        return notification
+
+    async def delete(self, notification: Notification) -> None:
+        await self.session.delete(notification)
+        await self.session.flush()
+
+    async def update(self, notification: Notification, notification_data: NotificationUpdateDTO) -> Notification:
+        update_data = notification_data.model_dump(exclude_unset=True)
+        for field, value in update_data.items():
+            setattr(notification, field, value)
+
+        await self.session.flush()
+        await self.session.refresh(notification)
+
+        return notification
+
+    async def get_all(self) -> list[Notification]:
+        stmt = select(Notification)
+        result = await self.session.execute(stmt)
+        return result.scalars().all()
