d32d1135f79cc54ba324f2e37fb87dc57735f886
Alexey <idolexey@yandex.ru>
2026-03-05 03:00:20 +0500

    Small user repo fixes

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -13,9 +13,11 @@ class UserRepository:
         self.session = session
 
     async def get_by_id(self, user_id: int) -> Optional[User]:
+        """Получить пользователя по ID"""
         return await self.session.get(User, user_id)
 
     async def create(self, user_data: UserCreateDTO) -> User:
+        """Создать пользователя"""
         password_hash = hash_password(user_data.password)
         user_dict = user_data.model_dump(exclude={"password"})
 
@@ -28,10 +30,12 @@ class UserRepository:
         return user
 
     async def delete(self, user: User) -> None:
+        """Удалить пользователя"""
         await self.session.delete(user)
         await self.session.flush()
 
     async def update(self, user: User, user_data: UserUpdateDTO) -> User:
+        """Обновить данные пользователя"""
         update_data = user_data.model_dump(exclude_unset=True)
         for field, value in update_data.items():
             setattr(user, field, value)
@@ -42,6 +46,14 @@ class UserRepository:
         return user
 
     async def get_all(self) -> list[User]:
+        """Получить всех пользователей"""
         stmt = select(User)
         result = await self.session.execute(stmt)
         return result.scalars().all()
+
+    async def get_by_field(self, field_name: str, value) -> Optional[User]:
+        """Получить первого пользователя, где поле field_name == value"""
+        column = getattr(User, field_name)
+        stmt = select(User).where(column == value)
+        result = await self.session.execute(stmt)
+        return result.scalars().first()
