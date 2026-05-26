43b253c140a9b21ce60d5fd01ef5317628067d15
adalarf <kirilsloboda@gmail.com>
2026-05-26 09:30:07 +0500

    fix challenge edit pydantic schemes

--- app/schemas/challenge.py
+++ app/schemas/challenge.py
@@ -22,7 +22,7 @@ class ChallengeReadDTO(BaseDTO):
     id: int
     title: str
     description: Optional[str] = None
-    url: Optional[AnyUrl] = None
+    url: Optional[str] = None
     status: ChallengeStatus
     date: Optional[datetime.date] = None
     max_points: Optional[int] = None
@@ -31,7 +31,8 @@ class ChallengeReadDTO(BaseDTO):
 class ChallengeUpdateDTO(BaseDTO):
     title: Optional[Annotated[str, MaxLen(255)]] = None
     description: Optional[Annotated[str, MaxLen(1000)]] = None
-    url: Optional[Annotated[AnyUrl, MaxLen(500)]] = None
+    # url: Optional[Annotated[AnyUrl, MaxLen(500)]] = None
+    url: Optional[str] = None
     status: Optional[ChallengeStatus] = None
     date: Optional[datetime.date] = None
     max_points: Optional[int] = None
